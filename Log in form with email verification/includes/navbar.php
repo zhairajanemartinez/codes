05585062a0e6291f70c5d23b679cac0ee6505288
zhairajanemartinez <zhairajanemartinez@gmail.com>
2024-09-18 15:09:54 +0800

    Add files via upload

--- Log in form with email verification/includes/navbar.php
+++ Log in form with email verification/includes/navbar.php
@@ -1,8 +1,8 @@
-<div class="bg-primary">
+<div class="bg-dark">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg bg-primary">
+                <nav class="navbar navbar-expand-lg navbar-dark">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
